716a28c4098523edd34a93aae2322e65bbb3f08f
Nico Mendoza <blankartwp@gmail.com>
2020-06-21 22:15:41 +0800

    responsiveness

--- components/Services.js
+++ components/Services.js
@@ -7,12 +7,40 @@ const Services = ({ data, ...args }) => {
   const slicedData = useMemo(() => {
     let newArr = [];
     if (data.length < 2) return [data];
-    for (var i = 0; i * 2 <= data.length; i++)
-      newArr.push(data.slice((i - 1) * 2, i * 2));
+    for (var i = 0; i < data.length; i += 2) newArr.push(data.slice(i, i + 2));
     return newArr;
   }, [data]);
+  const largeSlicedData = useMemo(() => {
+    let newArr = [];
+    if (data.length < 4) return [data];
+    for (var i = 0; i < data.length; i += 4) newArr.push(data.slice(i, i + 4));
+    return newArr;
+  }, [data]);
+
   return (
     <React.Fragment>
+      {largeSlicedData &&
+        size === "large" &&
+        largeSlicedData.map((newData) => (
+          <Box
+            flex={true}
+            align="center"
+            justify="center"
+            width="xxlarge"
+            direction={size === "large" || size === "medium" ? "row" : "column"}
+            gap="xlarge"
+          >
+            {newData &&
+              newData.map((service) => (
+                <ServiceItem
+                  key={service.title}
+                  src={service.image}
+                  title={service.title}
+                  content={service.content}
+                />
+              ))}
+          </Box>
+        ))}
       {slicedData &&
         size !== "large" &&
         slicedData.map((newData) => (
@@ -35,26 +63,6 @@ const Services = ({ data, ...args }) => {
               ))}
           </Box>
         ))}
-      {data && size === "large" && (
-        <Box
-          flex={true}
-          align="center"
-          justify="center"
-          width="xxlarge"
-          direction={size === "large" || size === "medium" ? "row" : "column"}
-          gap="xlarge"
-        >
-          {data &&
-            data.map((service) => (
-              <ServiceItem
-                key={service.title}
-                src={service.image}
-                title={service.title}
-                content={service.content}
-              />
-            ))}
-        </Box>
-      )}
     </React.Fragment>
   );
 };
