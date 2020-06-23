002de876d90e6a74162b0259c31492ba15819996
Renee Sarmiento <blankartwp@gmail.com>
2020-06-23 16:55:58 +0800

    fixed footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
 import Wrapper from "./Wrapper";
 import PageSection from "./PageSection";
-import { Text } from "grommet";
-import Link from "next/link";
+import { ResponsiveContext, Text } from "grommet";
+import { useContext } from "react";
 
 const Footer = ({ ...args }) => {
+  const size = useContext(ResponsiveContext);
   return (
     <PageSection
       pad={{ top: "large" }}
@@ -13,7 +14,13 @@ const Footer = ({ ...args }) => {
       {...args}
     >
       <Wrapper justify="end">
-        <Text color="background-front">
+        <Text
+          color="background-front"
+          style={{
+            fontSize:
+              size === "large" ? "20px" : size === "medium" ? "15px" : "10px",
+          }}
+        >
           Copyright © 2020 Blankart Designs. All Rights Reserved. Visit my{" "}
           <a
             href="https://www.upwork.com/freelancers/~01920e7fc00e054f37"
